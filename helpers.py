f8b8d04a67bfc570b47a4a9898971d40549a1913
Pavel Antonov <pvantonov@gmail.com>
2017-03-06 14:30:01 +0700

    Add ability to choose preferable subtitle language

---
+++ helpers.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+"""
+Helpers.
+"""
+from resources.lib.enum import Enum
+
+__all__ = ['Singleton']
+
+
+class Language(Enum):
+    """
+    Language enum.
+    """
+    Unknown = 0
+    Russian = 1
+    English = 2
+
+
+class Singleton(type):
+    """
+    Singleton metaclass.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
